a38e49f71b63c1f96732324eaa8e28d946c82a29
Salam Fazil <salaamfazil@icloud.com>
2024-03-04 12:11:47 -0800

    conflicts

--- nutridine/src/App.js
+++ nutridine/src/App.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import TetrisLoader from "./screens/loading/TetrisLoader";
-<<<<<<< HEAD
 import NavBar from "./components/navbar/Navbar";
 import FoodCardTest from "./components/FoodDisplay/FoodCardTest";
 import FoodCardList from "./components/FoodDisplay/FoodCardList";
 // import FoodCard from "./components/FoodCard/FoodCard";
-=======
 import MacroPage from "./screens/macro/MacroPage";
 import FoodCard from "./components/FoodCard";
 import Layout from "./Layout";
->>>>>>> b35ebd49ea0eb148535c5020717bf09aeee6dd68
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
