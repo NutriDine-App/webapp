d1ffc9ba033ab75ad861c3e7773945f35788e92e
John <john.mic.mckinnon@gmail.com>
2024-03-05 09:28:20 -0800

    Fix infinite loop fetch in useMealsByMacros

--- nutridine/src/hooks/useMealsByMacros.js
+++ nutridine/src/hooks/useMealsByMacros.js
@@ -61,14 +61,34 @@ export const fetchMealsByMacros = async ({
 };
 
 // Hook using the fetch function
-const useMealsByMacros = (params) => {
+const useMealsByMacros = ({
+  query,
+  minCalories,
+  maxCalories,
+  minProtein,
+  maxProtein,
+  minCarbs,
+  maxCarbs,
+  minFat,
+  maxFat,
+}) => {
   const [meals, setMeals] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data, error } = await fetchMealsByMacros(params);
+      const { data, error } = await fetchMealsByMacros({
+        query,
+        minCalories,
+        maxCalories,
+        minProtein,
+        maxProtein,
+        minCarbs,
+        maxCarbs,
+        minFat,
+        maxFat,
+      });
       if (data) {
         setMeals(data);
         setError(null);
@@ -79,7 +99,17 @@ const useMealsByMacros = (params) => {
     };
 
     fetchData();
-  }, [params]);
+  }, [
+    query,
+    minCalories,
+    maxCalories,
+    minProtein,
+    maxProtein,
+    minCarbs,
+    maxCarbs,
+    minFat,
+    maxFat,
+  ]);
 
   return { meals, loading, error };
 };
