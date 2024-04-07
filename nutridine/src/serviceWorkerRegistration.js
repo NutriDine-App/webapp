9dba919b8f163dba62d70f5464bd78c3635bb872
seyedmehradadimi <seyedmehradadimi@uvic.ca>
2024-04-07 12:30:07 -0700

    temp fix

--- nutridine/src/serviceWorkerRegistration.js
+++ nutridine/src/serviceWorkerRegistration.js
@@ -76,6 +76,7 @@ function registerValidSW(swUrl, config) {
               if (config && config.onUpdate) {
                 config.onUpdate(registration);
               }
+              installingWorker.postMessage({ type: "SKIP_WAITING" });
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
