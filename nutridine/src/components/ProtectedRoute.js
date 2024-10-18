a7686031b1986bc0e68667970b553927a352a4ae
Salam Fazil <salaamfazil@icloud.com>
2024-10-17 17:22:47 -0700

    restore protectedroute

--- nutridine/src/components/ProtectedRoute.js
+++ nutridine/src/components/ProtectedRoute.js
@@ -6,11 +6,10 @@ const ProtectedRoute = ({ children }) => {
     const { currentUser } = useAuth();
     const location = useLocation();
 
-    // Temporarily disable
-    // if (!currentUser) {
-    //     // Redirect to the login page, but save the current location they were trying to go to
-    //     return <Navigate to="/" state={{ from: location }} replace />;
-    // }
+    if (!currentUser) {
+        // Redirect to the login page, but save the current location they were trying to go to
+        return <Navigate to="/" state={{ from: location }} replace />;
+    }
 
     return children;
 };
